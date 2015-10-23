8b5c96764791c28667eb6a45f83c0103424e77eb
bhupinderkindlebit <bhupinderkindlebit@kindlebit.com>
2015-10-23 18:36:51 +0530

    Design created and starting implementation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,24 +12,22 @@ namespace ULDCustomer.Controllers
     public class HomeController : Controller
     {
         ULDQuestionService _obj = new ULDQuestionService();
+        List<Answer> answer = null;
         public ActionResult Index()
         {
             int ID = 1;
-
             //Insert into ULDSurvey first time
-
             vmULDQuestion model=new vmULDQuestion();
             ULDSurvey _uldservey = new ULDSurvey();
             model.question= _obj.GetQuestionByID(ID);
             model.SurveyID = _obj.AddServey();
-            List<Answer> answer = new List<Answer>();
+            answer = new List<Answer>();
             answer.Add(new Answer { AnswerId = 1, AnswerText = "Answer A" });
             answer.Add(new Answer { AnswerId = 2, AnswerText = "Answer B" });
             answer.Add(new Answer { AnswerId = 3, AnswerText = "Answer C" });
             answer.Add(new Answer { AnswerId = 4, AnswerText = "Answer D" });
             model.Answers = answer;
-            ViewBag.ID = ID;
-            
+            ViewBag.ID = ID;            
             return View(model);
         }
 
@@ -38,9 +36,6 @@ namespace ULDCustomer.Controllers
         {
             try
             {
-                //ID++;
-                //string s = Request.Form["choices"].ToString();
-
                 if (model.AnswerId == 1)
                 {
                     model.answerText = "A";
@@ -57,15 +52,12 @@ namespace ULDCustomer.Controllers
                 {
                     model.answerText = "D";
                 }
-                int res = _obj.AddQuestion(model);
-
+                _obj.AddQuestion(model);
                 if (model.ULDQuestionID < 4)
                 {
-
-                    int QuestionID = model.ULDQuestionID + 1;
-                    model.question = _obj.GetQuestionByID(QuestionID);
-                    ViewBag.ID = QuestionID;
-                    List<Answer> answer = new List<Answer>();
+                    ViewBag.ID = model.ULDQuestionID + 1;
+                    model.question = _obj.GetQuestionByID(ViewBag.ID);                 
+                    answer = new List<Answer>();
                     answer.Add(new Answer { AnswerId = 1, AnswerText = "Answer A" });
                     answer.Add(new Answer { AnswerId = 2, AnswerText = "Answer B" });
                     answer.Add(new Answer { AnswerId = 3, AnswerText = "Answer C" });
@@ -77,23 +69,18 @@ namespace ULDCustomer.Controllers
             }
             catch (Exception ex)
             { 
-
             }
             return View();            
-            
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
-
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
